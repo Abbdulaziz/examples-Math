dee0f4d559c91f9c4c374976e383b612e50f2e72
abbdulaziz <tabdulaziz313@gmail.com>
2022-10-25 15:28:38 +0500

    misol

---
+++ main.js
@@ -0,0 +1,276 @@
+"use strict"
+
+
+
+//  1-misol
+
+// let a = 158;
+// let b = a/100;
+// console.log(b);
+
+
+// 2-mmisol
+
+
+// let a = 4596;
+
+// let b = a/1000;
+// console.log(b);
+
+
+// 3-misol
+
+// let a = 58;
+// let b  = Math.floor(a/10);
+// let d = a%10;
+
+// console.log('onliklar ' + b + ' birliklar ' + d);
+
+// 4-misol
+
+// let a = 58;
+// let b  = Math.floor(a/10);
+// let d = a%10;
+
+// console.log(b+d);
+
+
+
+// 5-misol
+
+
+// let a = 58;
+// let b  = Math.floor(a/10);
+// let d = a%10;
+
+// console.log(b*d);
+// 6-misol
+
+// 7-misol
+
+// let a = 1205559998;
+// let q = a%1000;
+
+
+// console.log(q);
+
+// 8-misol
+
+
+// let a = 4896;
+// let d = a/60;
+// let q = a% 60;
+
+// let s = Math.floor( d/60);
+
+
+// console.log( 'soat ' +s+ " minut " +  d + ' sekund '+ a );
+
+
+// ------------------ if else----------------
+// 9-misol
+
+// let a = -95;
+
+// if(a>0){
+//     console.log('kotta');
+// }else{
+//     console.log('kichik');
+// }
+
+
+
+// 10-misol
+
+// let a = 16;
+
+// if(a%2){
+//     console.log('rost')
+// }else{
+//     console.log('yolgon');
+// }
+
+
+
+// 11-misol
+
+
+// let a = 16;
+
+// if(a%2){
+//     console.log('yolgon')
+// }else{
+//     console.log('rost');
+// }
+
+// 12-misol
+
+
+// let a = -65;
+
+// let b = 52;
+
+
+// if(a>0&&b>0){
+//     console.log('rost');
+// }else{
+//     console.log('yolgon');
+// }
+
+// 13-misol
+
+// let a = 98;
+// let b = 69;
+// let d = 18;
+
+
+// if ( a>b && b>d ){
+//     console.log('rost');
+// }else{
+//     console.log('yolgon');
+// }
+
+
+// 14-misol 
+// let a = 144;
+// let b = 7;
+// if (a % b == 1) {
+//     console.log('dushanba');
+// } else if (a % b == 2) {
+//     console.log('seshanba');
+// } else if (a % b == 3) {
+//     console.log('chorshanba');
+// } else if (a % b == 4) {
+//     console.log('payshanba');
+// } else if (a % b == 5) {
+//     console.log('juma');
+// } else if (a % b == 6) {
+//     console.log('shanba');
+// } else{
+//     console.log('yakshanba');
+// }
+
+
+// 15-masala
+
+// let a = 8;
+// let b = 9; 
+// let c = 10;
+
+// if( b >a && b<c ){
+//     console.log('rost')
+// }else{
+//     console.log('yolgon');
+// }
+
+
+// 16-misol
+
+// let a = 55;
+
+// let b = 65;
+
+// if( a%2==1 && b%2==1 ){
+//     console.log('rost');
+// }else{
+//     console.log('yolgon');
+// }
+
+
+// 17-misol
+
+// let a = 5;
+// let b = 6;
+// let d = 9;
+// let c = 7;
+
+
+// if( (a&&b&&c&&d)>0 ){
+//     console.log('rost')
+// }else{
+//     console.log('yolgon');
+// }
+
+// 18-misol
+// let a = 5;
+// let b = -8;
+// let c = -1;
+
+
+// if ( (a >0 && b>0&& c>0 ) || (  a <0 && b>0&& c>0 ) || (a >0 && b<0 && c>0 ) || (a >0 && b>0&& c<0 )  ){
+//     console.log('rost');
+// }
+// else{
+//     console.log('yolgon');
+// }
+
+
+// 19-misol
+
+
+// let a = 92;
+
+// if ( 90==a || a==91 ){
+//     console.log('bilayn');
+// } else if ( 93==a || a==94 ){
+//     console.log('usel');
+// }else if ( 95==a || a==99 ){
+//     console.log('uzmabile');
+// }else if ( 98==a  ){
+//     console.log('perfectom');
+// }else if ( 97==a  ){
+//     console.log('bilayn');
+// }else if ( a==77 ){
+//     console.log('humans');
+// }else{
+//     console.log('bunday companiya yoq');
+// }
+
+
+// 20-misol
+
+
+// let a = 9;
+
+// let b = 23;
+
+// if (a>b){
+//     console.log(a);
+// }else{
+//     console.log(b);
+// }
+
+
+
+
+// 21-misol
+
+// let a = 95;
+// let b = 918;
+// let c = 9;
+
+// if ( a >b && a>c ){
+//     console.log(a);
+// }else if(  b>a && b>c){
+//     console.log(b);
+// } else{
+//     console.log(c);
+// }
+
+// 22-misol
+
+
+let a = 95;
+let b = 918;
+let c = 9;
+
+if ( a <b && a<c ){
+    console.log(a);
+}else if(  b<a && b<c){
+    console.log(b);
+} else{
+    console.log(c);
+}
+
+
+
